77bf52d1e1f1ac09ee7b53de38cf1b672342035a
Dave Smith <davesmith.00000@gmail.com>
2021-11-02 21:05:13 +0000

    Split out the scene class into it's own file

--- src/main/g8/src/main/scala/$organization__packaged$/$module_name$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$module_name$.scala
@@ -5,13 +5,13 @@ import indigo.scenes._
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @JSExportTopLevel("IndigoGame")
-object $module_name$ extends IndigoGame[Unit, Unit, Unit, Unit] {
+object $module_name$ extends IndigoGame[Unit, Unit, Unit, Unit]:
 
   def initialScene(bootData: Unit): Option[SceneName] =
     None
 
   def scenes(bootData: Unit): NonEmptyList[Scene[Unit, Unit, Unit]] =
-    NonEmptyList(EmptyScene)
+    NonEmptyList(GameScene)
 
   val eventFilters: EventFilters =
     EventFilters.Permissive
@@ -41,51 +41,3 @@ object $module_name$ extends IndigoGame[Unit, Unit, Unit, Unit] {
 
   def present(context: FrameContext[Unit], model: Unit, viewModel: Unit): Outcome[SceneUpdateFragment] =
     Outcome(SceneUpdateFragment.empty)
-
-}
-
-object EmptyScene extends Scene[Unit, Unit, Unit] {
-
-  type SceneModel     = Unit
-  type SceneViewModel = Unit
-
-  val name: SceneName =
-    SceneName("empty")
-
-  val modelLens: Lens[Unit, Unit] =
-    Lens.keepLatest
-
-  val viewModelLens: Lens[Unit, Unit] =
-    Lens.keepLatest
-
-  val eventFilters: EventFilters =
-    EventFilters.Permissive
-
-  val subSystems: Set[SubSystem] =
-    Set()
-
-  def updateModel(context: FrameContext[Unit], model: Unit): GlobalEvent => Outcome[Unit] =
-    _ => Outcome(model)
-
-  def updateViewModel(
-      context: FrameContext[Unit],
-      model: Unit,
-      viewModel: Unit
-  ): GlobalEvent => Outcome[Unit] =
-    _ => Outcome(viewModel)
-
-  def present(context: FrameContext[Unit], model: Unit, viewModel: Unit): Outcome[SceneUpdateFragment] =
-    Outcome(
-      SceneUpdateFragment(
-        Shape
-          .Box(
-            Rectangle(0, 0, 60, 60),
-            Fill.LinearGradient(Point(0), RGBA.Magenta, Point(45), RGBA.Cyan)
-          )
-          .withRef(30, 30)
-          .moveTo(100, 100)
-          .rotateTo(Radians.fromSeconds(context.running * 0.25))
-      )
-    )
-
-}
